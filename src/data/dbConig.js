208d6e96242c4ddb62d065f80f7d2f5eae8af4a1
unknown <alanperez9six@gmail.com>
2021-01-17 20:54:13 -0600

    db config

---
+++ src/data/dbConig.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+const knex = require('knex');
+const dbEnv = process.env.NODE_ENV || 'development'
+const knexConfig = require('../../knexfile')[dbEnv]
+module.exports = knex(knexConfig);
