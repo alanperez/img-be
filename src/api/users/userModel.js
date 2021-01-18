fdd8b0e6ff5a5a02d8a88b27e1e002b606a3cbb1
unknown <alanperez9six@gmail.com>
2021-01-17 21:06:47 -0600

    returns first user by id

--- src/api/users/userModel.js
+++ src/api/users/userModel.js
@@ -2,8 +2,16 @@ const db = require('../../data/dbConig')
   const findAll = async () => {
     return await db('users').select('username','id')
 }
-
+const getById = async (id) => {
+    return await db('users').where({ id }).first();
+}
+const insertUser = async (user) => {
+    const [id] = await db('users').insert(user, 'id');
+    return getById(id)
+}
 module.exports = { 
     findAll,
+    insertUser,
+    getById
 
 }
