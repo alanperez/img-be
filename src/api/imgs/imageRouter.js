d43c619bd1b96d9737e9b672ec3483d00f0b15b3
unknown <alanperez9six@gmail.com>
2021-01-17 21:05:24 -0600

    pased in the logic after imported., retrieves all info from specific image

--- src/api/imgs/imageRouter.js
+++ src/api/imgs/imageRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-const { retrieveJoinedImages } = require('./imageController')
+const { retrieveJoinedImages,retrieveImageByID } = require('./imageController')
 
 const router = express.Router();
 
 // view all images
 router.get('/', retrieveJoinedImages)
 
-
+router.get('/:id', retrieveImageByID)
 module.exports = router;
