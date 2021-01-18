827cfebb01536f8e49d686e4c23d77e56d75e37e
unknown <alanperez9six@gmail.com>
2021-01-17 19:50:32 -0600

    local db

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  
+  development: {
+    client: 'mysql',
+    connection: {
+      database: 'shopifybe',
+      user: 'user',
+      password: 'password',
+      host: 'localhost',
+      port: 3306
+    } ,
+    migrations: { directory: 'src/data/migrations' },
+    seeds: { directory: 'src/data/seeds' },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  }
+
+};
