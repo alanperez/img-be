319e8c43bd9e74d9885084f7992be171bc4d58a2
unknown <alanperez9six@gmail.com>
2021-01-17 20:18:48 -0600

    jwt, register, login users

--- src/api/users/userController.js
+++ src/api/users/userController.js
@@ -1,4 +1,55 @@
+import generateToken from '../auth/generateToken'
 import Users from './userModel'
+import Images from '../imgs/imageModel'
+import bcrypt from 'bcrypt'
+
+
+
+async function registerUser(req,res) {
+    try {
+        const { username, password } = req.body;
+        const rounds = 8;
+        if (username && password) {
+          const salt = bcrypt.genSaltSync(parseInt(rounds));
+          const hash = bcrypt.hashSync(password, salt);
+          const user = {
+            username,
+            password: hash
+          };
+          console.log(user);
+          const createdUser = await Users.insertUser(user);
+          const token = await generateToken(createdUser.username)
+          res.status(201).json({...createdUser, token});
+        } else {
+          res.status(409).json({ error: 'unrecognized fields' });
+        }
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'something went wrong' });
+      }
+}
+
+async function loginUser(req,res) {
+    let { username, password } = req.body;
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)){
+          const token = generateToken(user);
+          res.status(200).json({
+            token,
+            message: `Welcome ${user.username}!`,
+            id: user.id
+          })
+        } else {
+          res.status(401).json({message: 'Invalid Credentials'})
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+}
+
 
 // Retrieves all Users ( username & id)
 async function retrieveUsers(req,res) {
@@ -12,4 +63,19 @@ async function retrieveUsers(req,res) {
     })
   }
   
-module.exports = { retrieveUsers }
+  async function userViewOwnImages(req,res) {
+    const username = req.decoded.username
+    Images.lookByID(req.decoded.id)
+    // console.log('clg req params iddd', req.params.id)
+    .then(image => {
+      // console.log('clg da img.d', image)
+      res.status(200).json(image);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        message: "There was an error while retreiving the parent's children. Please try again later."
+      })
+    })
+  }
+module.exports = { loginUser,registerUser,retrieveUsers, userViewOwnImages }
--- src/api/users/userRouter.js
+++ src/api/users/userRouter.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import {retrieveUsers} from './userController'
+import restrict from '../auth/restrict'
+import {retrieveUsers, userViewOwnImages} from './userController'
 
 const router = express.Router();
 
 
 router.get('/', retrieveUsers);
 
+// retrieves all images of specific user and verifies the token
+// the users view their images that they have uploaded
 
+router.get('/view/uploaded-images',restrict, userViewOwnImages )
+router.post('/register', registerUser)
+router.post('/login', loginUser)
 
 module.exports = router;
