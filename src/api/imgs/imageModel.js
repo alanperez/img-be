2040b91e132a7996d600933acb30a8a0852fe6ad
unknown <alanperez9six@gmail.com>
2021-01-17 20:50:08 -0600

    db config

--- src/api/imgs/imageModel.js
+++ src/api/imgs/imageModel.js
@@ -1,3 +1,12 @@
+const db = require('../../data/dbConig')
+const JoinFields = [
+    'images.id',
+    'images.image_url',
+    'images.tag',
+    'images.user_id',
+    'users.username'
+  ];
+
 
 const findBy = (filter) => {
     return db("users").where(filter);
@@ -10,11 +19,16 @@ function lookByID(user_id) {
     return db('images').where({user_id})
   }
 
-
+function getAll() {
+    return db('images')
+      .select(JoinFields)
+      .join('users', 'users.id', 'images.user_id');
+  }
 
 module.exports = {
     findBy,
     insertUser,
     lookByID,
+    getAll
 
 }
