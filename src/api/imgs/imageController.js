f05b5f1ccfd16b44c001f8885d7f1e1e6189d920
unknown <alanperez9six@gmail.com>
2021-01-17 21:04:35 -0600

    retrieve image by id, logic is in the controller

--- src/api/imgs/imageController.js
+++ src/api/imgs/imageController.js
@@ -12,4 +12,21 @@ async function retrieveJoinedImages(req,res) {
         })
 }
 
-module.exports = { retrieveJoinedImages }
+async function retrieveImageByID(req,res) {
+    const { id } = req.params;
+    try {
+      const image = await Images.getBy(id);
+      if (image) {
+        res.status(200).json(image);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'Problem with specified ID does not exist.' });
+      }
+    } catch (error) {
+      res
+        .status(404)
+        .json({ message: `The reason you're getting an error: ${error}` });
+    }
+}
+module.exports = { retrieveJoinedImages,retrieveImageByID }
