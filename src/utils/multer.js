c6c6a953b859763d9b78d5d122389a46dc5170d5
unknown <alanperez9six@gmail.com>
2021-01-17 23:55:08 -0600

    multer and cloudinary for file upload

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -6,7 +6,7 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
 
-        folder: "demo",
+        folder: "images",
         allowedFormats: ["jpg", "png"],
         transformation: [{ width: 500, height: 500, crop: "limit" }]
     }
