0eff56c0c92cf7fe4068dfc5c9323d3b1395c5be
unknown <alanperez9six@gmail.com>
2021-01-17 19:55:48 -0600

    images schema

--- src/data/migrations/20210117195424_images-table.js
+++ src/data/migrations/20210117195424_images-table.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex) {
+    return knex.schema
+      .createTable('images', (tbl) => {
+          tbl.increments();
+          tbl.integer('user_id')
+              .notNullable()
+              .unsigned()
+              .references('id')
+              .inTable('users')
+              .onDelete('CASCADE')
+              .onUpdate('CASCADE');
+          tbl.text('image_url').notNullable();
+          tbl.string('tag').notNullable();
+    })
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('images')
+  };
   
-};
