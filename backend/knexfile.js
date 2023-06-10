e309c5e6a9547e1a0b768eef720f14a9f69bb637
perezxalan <perezxalan@gmail.com>
2023-06-10 12:25:25 -0500

    setting up folders

---
+++ backend/knexfile.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    client: "mysql",
+    connection: {
+      database: "img-be",
+      user: "root",
+      password: "password",
+      host: "localhost",
+      port: 3306,
+    },
+    migrations: { directory: "src/data/migrations" },
+    seeds: { directory: "src/data/seeds" },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+  production: {
+    client: "mysql",
+    connection: {
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+    },
+    migrations: { directory: "src/data/migrations" },
+    seeds: { directory: "src/data/seeds" },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+};
